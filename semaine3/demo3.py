b2b8c1015cdbfa2f59207b89b75db6b5cd8f5248
BinLu à Montréal <130716168+Bunny2024@users.noreply.github.com>
2023-05-08 21:23:23 -0400

    Add files via upload

---
+++ semaine3/demo3.py
@@ -0,0 +1,32 @@
+counter = 0
+somme = 0
+while counter <= 10:
+    somme = somme + counter 
+    # if counter % 2 == 0:
+       # print(f"counter = {counter}")
+    # counter = counter + 1
+    counter += 1
+    # counter++
+    # print(f"counter ={counter} and somme = {somme}")
+print(f"somme = {somme}")
+print("The end")
+
+
+# counter = 0
+# number= int(input("Enter the value of number:"))
+# while counter <= number:
+#    print(f"counter = {counter}")
+#    if counter % 5 == 0:
+#        print(f"counter = {counter}")
+#    counter += 5
+# print("The end")
+
+#counter = 1
+#produit = 1
+#number= int(input("Enter the value of number:"))
+#while counter <= number:
+#    produit = produit * counter
+#    print(f"produit = {produit}")
+#    counter += 1
+#print(f"produit ={produit}")
+#print("The end")
---
+++ semaine3/demo4.py
@@ -0,0 +1,12 @@
+for counter in range(0,10,2):
+# for counter in (0,1,2,3,4):
+    print("Bonjour les ami.e.s")
+print("The end")
+
+somme = 0
+nomber = 153
+# 1er 
+last = nomber % 10
+somme = somme + last**3
+no_ber =nomber //10
+# 2e
