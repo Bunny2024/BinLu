b7c7d682af1c6f076823155136d78e7a77125cb2
BinLu à Montréal <130716168+Bunny2024@users.noreply.github.com>
2023-05-15 14:40:16 -0400

    Add files via upload

---
+++ semaine1/devoir5.py
@@ -0,0 +1,28 @@
+a = int(input("Enter A integer: "))
+b = int(input("Enter B integer: "))
+
+# 1
+sum = a + b
+print(f"La somme de a et b est{sum}")
+
+#2
+dif = a - b
+print(f"La différence est {dif}")
+
+#3
+produit = a * b 
+print(f"Le produit est{produit}")
+
+#4
+qoutient = a / b
+print(f"Le quotient a / b est{qoutient}")
+
+#5
+reste = a % b
+print(f"Le reste lorsque a est divisé par b est{reste}")
+
+#6
+
+#7
+result = a **b
+print(f"Le résultat de a**b est{result}")
