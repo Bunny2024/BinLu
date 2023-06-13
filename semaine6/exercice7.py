5c943fd66cc9b81ff09934f5d49565bfdc22d2f6
BinLu à Montréal <130716168+Bunny2024@users.noreply.github.com>
2023-06-13 10:03:44 -0400

    Delete semaine6 directory

--- semaine6/exercice7.py
+++
@@ -1,6 +0,0 @@
-def suppimer_rang(liste,rang):
-    n = len(liste)
-    new_liste = liste[ : rang] + liste[rang + 1:  ]
-    return new_liste
-print("suppimer rang")
-print(suppimer_rang([8,7,6,5,4],2))
