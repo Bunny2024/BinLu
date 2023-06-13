6a67bf0706efa6dfd17650f644ca54a9f5e612ce
BinLu à Montréal <130716168+Bunny2024@users.noreply.github.com>
2023-06-13 10:02:48 -0400

    Delete exercice2.py

--- semaine6/exercice2.py
+++
@@ -1,9 +0,0 @@
-# une liste d'équipes
-
-def nom_capitaine(list_teams):
-    return list_teams[1]
-
-
-list_teams=["Vézina"," Albert"," George"," Amos"]
-nom_capitaine = list_teams[1]
-print(nom_capitaine)
